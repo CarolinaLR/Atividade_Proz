abfe969c9fa1c4b88d25c56aaaa9ecc0aa314aac
CarolinaLR <62269821+CarolinaLR@users.noreply.github.com>
2024-03-13 15:53:21 -0300

    Add files via upload

---
+++ sprint.js
@@ -0,0 +1,14 @@
+nuemroDaSorte = [37, 14 ,26 ,5 ,94,87]
+ for(let i = 0; i < nuemroDaSorte.length; i++); {
+    if 
+    (nuemroDaSorte[i] % 2 == 0 && nuemroDaSorte [i] < 50 ){
+
+        console.log (nuemroDaSorte[i] + "é par e menor que 50.")
+    } 
+    else if (nuemroDaSorte [i] < 50 ) {
+        console.log( nuemroDaSorte[i] + "é menor que 50.")} 
+ else {
+    console.log(nuemroDaSorte[i] + "é maior que 50.")
+ }
+
+ }
